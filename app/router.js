39f7212deefc4cf9784ffbdc6dbab42e0a60aac0
gergokutu <gergo.kutu@gmail.com>
2020-10-26 09:40:37 +0100

    Config jsconfig.json

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'shopping-tut/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function() {
+});
