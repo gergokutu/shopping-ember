6d59c4c85c79d9b5f18853f44d9c6c32b425adf2
gergokutu <gergo.kutu@gmail.com>
2020-10-26 12:26:56 +0100

    Destroy routes > ember destroy route clothes/index && ember destroy route clothes/t-shirt && ember destroy route clothes

--- app/routes/item.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class ItemRoute extends Route {
-    model(params) {
-        // console.log(params)
-        const { item_id } = params;
-        return item_id;
-    }
-}
